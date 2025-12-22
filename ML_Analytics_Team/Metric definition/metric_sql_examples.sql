32c64a57f7e58801621710ed4a08a3e688c19246
Angad kumar <104129603+angadkumar9128@users.noreply.github.com>
2025-12-22 11:13:01 +0530

    Update metric_sql_examples.sql

--- ML_Analytics_Team/Metric definition/metric_sql_examples.sql
+++ ML_Analytics_Team/Metric definition/metric_sql_examples.sql
@@ -1,25 +1,27 @@
 -- Encounter count in last 30 days
 SELECT
   patient_id,
-  COUNT(*) AS encounter_count_30d
-FROM patient_encounter_summary
-WHERE encounter_date >= current_date() - INTERVAL 30 DAYS
+  COUNT(DISTINCT encounter_id) AS encounter_count_30d
+FROM angad_kumar91.fhir_healthcare_analytics_gold.patient_encounter_summary
+WHERE admit_time >= current_date() - INTERVAL 30 DAYS
 GROUP BY patient_id;
+
 
 -- Readmission flag (within 30 days)
 SELECT
   patient_id,
   CASE
-    WHEN COUNT(*) > 1 THEN 1
+    WHEN COUNT(DISTINCT encounter_id) > 1 THEN 1
     ELSE 0
   END AS readmission_flag
-FROM admissions_analytics
-WHERE admission_date >= current_date() - INTERVAL 30 DAYS
+FROM angad_kumar91.fhir_healthcare_analytics_gold.patient_encounter_summary
+WHERE admit_time >= current_date() - INTERVAL 30 DAYS
 GROUP BY patient_id;
+
 
 -- Total claim cost per patient
 SELECT
   patient_id,
-  SUM(claim_amount) AS total_claim_cost
-FROM claims_summary
+  SUM(total_amount) AS total_claim_cost
+FROM angad_kumar91.fhir_healthcare_analytics_gold.patient_encounter_summary
 GROUP BY patient_id;
