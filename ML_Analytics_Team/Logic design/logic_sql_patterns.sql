b19b60e252ebee0d588d4bbcc9d6d55cee015b3b
Angad kumar <104129603+angadkumar9128@users.noreply.github.com>
2025-12-22 11:26:48 +0530

    Update logic_sql_patterns.sql

--- ML_Analytics_Team/Logic design/logic_sql_patterns.sql
+++ ML_Analytics_Team/Logic design/logic_sql_patterns.sql
@@ -1,8 +1,9 @@
 -- Example: Utilization score normalization
 SELECT
   patient_id,
-  LEAST(encounter_count_30d * 10, 100) AS utilization_score
-FROM metric_table;
+  LEAST(total_encounters * 10, 100) AS utilization_score
+FROM angad_kumar91.fhir_healthcare_analytics_gold.patient_metrics;
+
 
 -- Example: Chronic burden score
 SELECT
@@ -13,15 +14,46 @@ SELECT
     WHEN chronic_condition_count >= 1 THEN 40
     ELSE 10
   END AS chronic_burden_score
-FROM metric_table;
+FROM angad_kumar91.fhir_healthcare_analytics_gold.patient_metrics;
+
 
 -- Example: Overall risk score
 SELECT
   patient_id,
-  (
+  ROUND(
     utilization_score * 0.30 +
     chronic_burden_score * 0.30 +
     cost_intensity_score * 0.25 +
-    readmission_score * 0.15
+    readmission_score * 0.15,
+    2
   ) AS overall_risk_score
-FROM feature_scores;
+FROM angad_kumar91.fhir_healthcare_analytics_gold.feature_scores;
+
+
+-- Run this for better result
+CREATE OR REPLACE VIEW angad_kumar91.fhir_healthcare_analytics_gold.feature_scores AS
+SELECT
+  patient_id,
+
+  LEAST(total_encounters * 10, 100) AS utilization_score,
+
+  CASE
+    WHEN chronic_condition_count >= 5 THEN 100
+    WHEN chronic_condition_count >= 3 THEN 70
+    WHEN chronic_condition_count >= 1 THEN 40
+    ELSE 10
+  END AS chronic_burden_score,
+
+  CASE
+    WHEN total_claim_cost >= 5000 THEN 100
+    WHEN total_claim_cost >= 2000 THEN 70
+    WHEN total_claim_cost >= 500 THEN 40
+    ELSE 10
+  END AS cost_intensity_score,
+
+  CASE
+    WHEN readmission_flag = 1 THEN 100
+    ELSE 20
+  END AS readmission_score
+
+FROM angad_kumar91.fhir_healthcare_analytics_gold.patient_metrics;
