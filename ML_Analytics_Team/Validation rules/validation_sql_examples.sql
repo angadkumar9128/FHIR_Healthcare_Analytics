3f285004dee9f729fd817f9c20b74d1134bd544a
Angad kumar <104129603+angadkumar9128@users.noreply.github.com>
2025-12-22 11:32:49 +0530

    Update validation_sql_examples.sql

--- ML_Analytics_Team/Validation rules/validation_sql_examples.sql
+++ ML_Analytics_Team/Validation rules/validation_sql_examples.sql
@@ -1,15 +1,22 @@
 -- Null and range validation
 SELECT *
-FROM metric_table
+FROM angad_kumar91.fhir_healthcare_analytics_gold.metric_table
 WHERE encounter_count_30d < 0
-   OR total_claim_cost < 0;
+   OR total_claim_cost < 0
+   OR encounter_count_30d IS NULL
+   OR total_claim_cost IS NULL;
+
 
 -- Score boundary validation
 SELECT *
-FROM feature_scores
-WHERE utilization_score NOT BETWEEN 0 AND 100;
+FROM angad_kumar91.fhir_healthcare_analytics_gold.feature_scores
+WHERE utilization_score NOT BETWEEN 0 AND 100
+   OR chronic_burden_score NOT BETWEEN 0 AND 100
+   OR cost_intensity_score NOT BETWEEN 0 AND 100
+   OR readmission_score NOT BETWEEN 0 AND 100;
 
 -- Flag unrealistic LOS
 SELECT *
-FROM admissions_analytics
+FROM angad_kumar91.fhir_healthcare_analytics_gold.admissions_analytics
 WHERE avg_los > 365;
+
